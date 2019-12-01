2beb1f12ca20f75b6d2addc49f76c945b48ddd9e
sylwiaZon <sylwia.zon@gmail.com>
2019-12-01 22:39:51 +0100

    Finail initial version of Backend

---
+++ PanBajgielBackend/src/main/java/com/Controllers/UserController.java
@@ -0,0 +1,11 @@
+package com.Controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class UserController {
+    @RequestMapping("/")
+    public String display()
+    {
+        return "index";
+    }
+}
