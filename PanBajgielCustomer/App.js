1c5bb7da3070676e41317bac51758e23c18d1726
agatakawalec <kawalecagata98@gmail.com>
2019-12-10 17:17:26 +0100

    Changed Default View (added headers)

--- PanBajgielCustomer/App.js
+++ PanBajgielCustomer/App.js
@@ -1,149 +1,199 @@
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
-
+import {ImageBackground, StyleSheet, Text, View, Image} from "react-native";
+import {createStackNavigator} from "react-navigation-stack";
+import {createAppContainer} from "react-navigation";
+import {createBottomTabNavigator} from "react-navigation-tabs";
 import Icon from "react-native-vector-icons/FontAwesome5";
 
 
+
 export default class App extends React.Component {
-  render() {
-    return (
-        <AppContainer />
-    );
-  }
+    render() {
+        return (
+            <AppContainer />
+        );
+    }
 }
+
 
 class HomeScreen extends React.Component {
-  render() {
-    return(
-     <View style={{ alignItems: 'center',justifyContent: 'center'}}>
+    render() {
+        return(
+            <View style={{ alignItems: 'center',justifyContent: 'center'}}>
 
-     <ImageBackground accessible={true} testID="HomeScreen" accessibilityLabel={'HomeScreen'} source={require('./assets/background.png')} style={styles.background}>
-    <Text>Banan głowny!</Text>
-  </ImageBackground>
-    </View>
-    );
-  }
+                <ImageBackground accessible={true} testID="HomeScreen" accessibilityLabel={'HomeScreen'} source={require('./assets/background.png')} style={styles.background}>
+                    <Text>Banan głowny!</Text>
+                </ImageBackground>
+            </View>
+        );
+    }
 }
 
+
 class ProfileScreen extends React.Component {
-  render() {
-    return(
-      <View  accessible={true} testID="ProfileScreen" accessibilityLabel={'ProfileScreen'} style={{ alignItems: 'center',justifyContent: 'center'}}>
-    <ImageBackground source={require('./assets/background.png')} style={styles.background}>
-    <Text>Jestem bananem i to moje bananowe dane!</Text>
-  </ImageBackground>
-    </View>
-    );
-  }
+    render() {
+        return(
+            <View  accessible={true} testID="ProfileScreen" accessibilityLabel={'ProfileScreen'} style={{ alignItems: 'center',justifyContent: 'center'}}>
+                <ImageBackground source={require('./assets/background.png')} style={styles.background}>
+                    <Text>Jestem bananem i to moje bananowe dane!</Text>
+                </ImageBackground>
+            </View>
+        );
+    }
 }
 
 class ProductsScreen extends React.Component {
-  render() {
-    return(
-      <View  accessible={true}  testID="ProductsScreen" accessibilityLabel={'ProductsScreen'} style={{ alignItems: 'center',justifyContent: 'center'}}>
-    <ImageBackground source={require('./assets/background.png')} style={styles.background}>
-    <Text>Lista bananów!</Text>
-  </ImageBackground>
-    </View>
-    );
-  }
+    render() {
+        return(
+            <View  accessible={true}  testID="ProductsScreen" accessibilityLabel={'ProductsScreen'} style={{ alignItems: 'center',justifyContent: 'center'}}>
+                <ImageBackground source={require('./assets/background.png')} style={styles.background}>
+                    <Text>Lista bananów!</Text>
+                </ImageBackground>
+            </View>
+        );
+    }
 }
 class PromotionsScreen extends React.Component {
-  render() {
-    return(
-      <View  accessible={true} testID="ProfileScreen" accessibilityLabel={'PromotionsScreen'} style={{ alignItems: 'center',justifyContent: 'center'}}>
-    <ImageBackground source={require('./assets/background.png')} style={styles.background}>
-    <Text>Lista babanowych promocji :D!</Text>
-  </ImageBackground>
-    </View>
-    );
-  }
+    render() {
+        return(
+            <View  accessible={true} testID="ProfileScreen" accessibilityLabel={'PromotionsScreen'} style={{ alignItems: 'center',justifyContent: 'center'}}>
+                <ImageBackground source={require('./assets/background.png')} style={styles.background}>
+                    <Text>Lista babanowych promocji 😀!</Text>
+                </ImageBackground>
+            </View>
+        );
+    }
 }
 class SettingsScreen extends React.Component {
-  render() {
-    return(
-      <View  accessible={true} testID="SettingsScreen" accessibilityLabel={'SettingsScreen'} style={{ alignItems: 'center',justifyContent: 'center'}}>
-    <ImageBackground source={require('./assets/background.png')} style={styles.background}>
-    <Text>ja ustawiam banany!</Text>
-  </ImageBackground>
-    </View>
-    );
-  }
-}
-const bottomTabNavigator = createBottomTabNavigator(
-  {
-  	Settings: {
-  		screen: SettingsScreen,
-  		navigationOptions:{
-  			tabBarIcon: ({ tintColor }) => (
-
-          <Icon  name="cog" size={36} color={tintColor}  accessible={true}  testID="settingsIcon" accessibilityLabel={'settingsIcon'} />
-        )
-  		}
-
-  	},
-
-  	Products: {
-  		screen: ProductsScreen,
-  		navigationOptions:{
-  			tabBarIcon: ({ tintColor }) => (
-           <Image  source={(tintColor=='#a9ecf3') ? require( './assets/ikonaFocus.png')
-                                : require( './assets/ikona.png')} style={{width:50, height:50}}  accessible={true} testID="productsIcon" accessibilityLabel={'productsIcon'} />
-        )
-  		}
-  	},
-
-  	Promotions: {
-  		screen: PromotionsScreen,
-  		navigationOptions:{
-  			tabBarIcon: ({ tintColor }) => (
-          <Icon  name="percentage" size={36} color={tintColor}  accessible={true}  testID="promotionsIcon" accessibilityLabel={'promotionsIcon'}/>
-        )
-  		}
-  	},
-
-    Home: {
-      screen: HomeScreen,
-      navigationOptions: {
-        tabBarIcon: ({ tintColor }) => (
-          <Icon name="home" size={36} color={tintColor}  accessible={true}  testID="homeIcon" accessibilityLabel={'homeIcon'} />
-        )
-      }
-    },
-
-    Profile: {
-      screen: ProfileScreen,
-      navigationOptions: {
-        tabBarIcon: ({ tintColor }) => (
-          <Icon name="user" size={36} color={tintColor}  accessible={true} testID="userIcon" accessibilityLabel ={'userIcon'}  />
-        )
-      }
+    render() {
+        return(
+            <View  accessible={true} testID="SettingsScreen" accessibilityLabel={'SettingsScreen'} style={{ alignItems: 'center',justifyContent: 'center'}}>
+                <ImageBackground source={require('./assets/background.png')} style={styles.background}>
+                    <Text>ja ustawiam banany!</Text>
+                </ImageBackground>
+            </View>
+        );
     }
-  },
-  {
-    initialRouteName: 'Home',
-    tabBarOptions: {
-    inactiveTintColor: '#fff',
-      activeTintColor: '#a9ecf3',
-      style: {
+}
+
+const HomeStack = createStackNavigator({Home: HomeScreen, },
+    {defaultNavigationOptions: {
+            title: 'Home',
+            headerStyle: {
+                backgroundColor: '#55858A'
+            },
+            headerTintColor: '#FFFFFF',
+            headerTitleStyle: {
+                textAlign:"center",
+                flex:1
+            },}})
+
+const ProfileStack = createStackNavigator({Profile: ProfileScreen, },
+    {defaultNavigationOptions: {
+            title: 'Profile',
+            headerStyle: {
+                backgroundColor: '#55858A'
+            },
+            headerTintColor: '#FFFFFF',
+            headerTitleStyle: {
+                textAlign:"center",
+                flex:1
+            },}})
+
+const PromotionsStack = createStackNavigator({Promotions: PromotionsScreen, },
+    {defaultNavigationOptions: {
+            title: 'Promotion',
+            headerStyle: {
+                backgroundColor: '#55858A'
+            },
+            headerTintColor: '#FFFFFF',
+            headerTitleStyle: {
+                textAlign:"center",
+                flex:1
+            },}})
+
+const SettingsStack = createStackNavigator({Settings: SettingsScreen, },
+    {defaultNavigationOptions: {
+            title: 'Settings',
+            headerStyle: {
+                backgroundColor: '#55858A'
+            },
+            headerTintColor: '#FFFFFF',
+            headerTitleStyle: {
+                textAlign:"center",
+                flex:1
+            },}})
+
+const ProductsStack = createStackNavigator({Products: ProductsScreen, },
+    {defaultNavigationOptions: {
+            title: 'Products',
+            headerStyle: {
+                backgroundColor: '#55858A'
+            },
+            headerTintColor: '#FFFFFF',
+            headerTitleStyle: {
+                textAlign:"center",
+                flex:1
+            },}})
+
+
+const AppContainer = createAppContainer(createBottomTabNavigator(
+    {
+        Home: HomeStack,
+        Profile: ProfileStack,
+        Products: ProductsStack,
+        Promotions: PromotionsStack,
+        Settings: SettingsStack
+    },
+    {
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName, iconId, iconLabel, flag = true;
+                if (routeName === 'Home') {
+                    iconName = "home",
+                        iconId = "settingsIcon",
+                        iconLabel = 'settingsIcon'
+                } else if (routeName === 'Settings') {
+                    iconName = "cog",
+                        iconId = "settingsIcon",
+                        iconLabel = 'settingsIcon'
+                } else if (routeName === 'Products') {
+                    iconName = "cog",
+                        iconId = "productsIcon",
+                        iconLabel = 'productsIcon',
+                        flag = false
+                } else if (routeName === 'Promotions'){
+                    iconName = "percentage",
+                        iconId = "promotionsIcon",
+                        iconLabel = 'promotionsIcon'
+                } else if (routeName === 'Profile'){
+                    iconName = "user",
+                        iconId = "profileIcon",
+                        iconLabel = 'profileIcon'
+                }
+                return flag ? <Icon name={iconName} size={36} color={tintColor}  accessible={true} testID={iconId} accessibilityLabel ={iconLabel}  /> : <Image  source={(tintColor=='#a9ecf3') ? require( './assets/ikonaFocus.png')
+                    : require( './assets/ikona.png')} style={{width:50, height:50}}  accessible={true} testID="productsIcon" accessibilityLabel={'productsIcon'} />;
+            },
+        }),
+        tabBarOptions: {
+            inactiveTintColor: '#fff',
+            activeTintColor: '#a9ecf3',
+            style: {
                 backgroundColor: 'rgba(106,147,151,1)',
             },
             showLabel: false
+        }
     }
-  }
-);
+));
 
-const AppContainer = createAppContainer(bottomTabNavigator);
 
 const styles = StyleSheet.create({
-background: {
-	width: '100%', 
-	height: '100%',
-	alignItems: 'center',
-	justifyContent: 'center',
-	opacity:0.8
-}
-
+    background: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        opacity:0.8
+    }
 });
