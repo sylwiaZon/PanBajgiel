6a2410e05404bd10532885f1a477d80a03da78cb
agatakawalec <kawalecagata98@gmail.com>
2019-12-28 01:16:02 +0100

    Default view

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import Navigation from './src/navigation/AppNavigation';
+
+export default function App() {
+  return (
+      <View style={styles.container}>
+        <Navigation/>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
