1f44844895ef544eec6cb65d6f78b63a2c94fb95
Ania Wójcik <34057261+wojcikania12@users.noreply.github.com>
2020-01-11 23:53:28 +0100

    Add files via upload

---
+++ PanBajgielAssistant/src/navigation/AppNavigation.js
@@ -0,0 +1,256 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createSidebarNavigator } from '../tabs';
+import {View, Text, Dimensions, ImageBackground, StyleSheet}from "react-native";
+import {Header} from  'react-native-elements'
+import {createStackNavigator} from "react-navigation-stack";
+import {Transaction} from "./Transaction.js";
+
+
+var {width, height} = Dimensions.get('window');
+var formattedDate = new Date();
+var newDate = formattedDate.getDay().toString() + "/" + formattedDate.getMonth().toString() + "/" + formattedDate.getFullYear().toString();
+
+const styles = StyleSheet.create({
+    header: {
+        marginRight: 0.05 * width,
+        color: 'white',
+        fontSize: 0.02 * height,
+    },
+    background: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        opacity:0.8
+    }
+});
+
+class ProfileScreen extends React.Component {
+    render() {
+        return(
+            <View style={{ alignItems: 'center',justifyContent: 'center'}}>
+                <ImageBackground accessible={true} testID="HomeScreen" accessibilityLabel={'HomeScreen'} source={require('../../assets/background.png')} style={styles.background}>
+                <Header
+                    centerComponent={{ text: 'UŻYTKOWNIK', style: { flex:1, color: '#fff', fontSize: 0.02 * height, alignItems: 'center',
+                            justifyContent: 'center',}}}
+                    containerStyle={{
+                        backgroundColor: '#bef7e8',
+                        height: 0.06 * height
+                    }}
+                />
+                </ImageBackground>
+            </View>
+        );
+    }
+}
+
+
+class TransactionScreen extends React.Component {
+    render() {
+        return(
+            <View  accessible={true} testID="ProfileScreen" accessibilityLabel={'TransactionScreen'} style={{ alignItems: 'center',justifyContent: 'center'}}>
+                <ImageBackground accessible={true} testID="HomeScreen" accessibilityLabel={'HomeScreen'} source={require('../../assets/background.png')} style={styles.background}>
+                <Header
+                    centerComponent={{ text: 'TRANSAKCJA', style: { flex:1, color: '#fff', fontSize: 0.02 * height, alignItems: 'center',
+                            justifyContent: 'center',}}}
+                    containerStyle={{
+                        backgroundColor: '#bef7e8',
+                        height: 0.06 * height
+                    }}
+                />
+                <Transaction/>
+                </ImageBackground>
+            </View>
+        );
+    }
+}
+
+class StatsScreen extends React.Component {
+    render() {
+        return(
+            <View  accessible={true}  testID="ProductsScreen" accessibilityLabel={'StatsScreen'} style={{ alignItems: 'center',justifyContent: 'center'}}>
+                <ImageBackground accessible={true} testID="HomeScreen" accessibilityLabel={'HomeScreen'} source={require('../../assets/background.png')} style={styles.background}>
+                <Header
+                    centerComponent={{ text: 'STATYSTYKI', style: { flex:1, color: '#fff', fontSize: 0.02 * height, alignItems: 'center',
+                            justifyContent: 'center',}}}
+                    containerStyle={{
+                        backgroundColor: '#bef7e8',
+                        height: 0.06 * height
+                    }}
+                />
+                </ImageBackground>
+            </View>
+        );
+    }
+}
+class PricesScreen extends React.Component {
+    render() {
+        return(
+            <View  accessible={true} testID="ProfileScreen" accessibilityLabel={'PricesScreen'} style={{ alignItems: 'center',justifyContent: 'center'}}>
+                <ImageBackground accessible={true} testID="HomeScreen" accessibilityLabel={'HomeScreen'} source={require('../../assets/background.png')} style={styles.background}>
+                <Header
+                    centerComponent={{ text: 'CENNIK', style: { flex:1, color: '#fff', fontSize: 0.02 * height, alignItems: 'center',
+                            justifyContent: 'center',}}}
+                    containerStyle={{
+                        backgroundColor: '#bef7e8',
+                        height: 0.06 * height
+                    }}
+                />
+                </ImageBackground>
+            </View>
+        );
+    }
+}
+class SettingsScreen extends React.Component {
+    render() {
+        return(
+            <View  accessible={true} testID="SettingsScreen" accessibilityLabel={'SettingsScreen'}>
+                <ImageBackground accessible={true} testID="HomeScreen" accessibilityLabel={'HomeScreen'} source={require('../../assets/background.png')} style={styles.background}>
+                <Header
+                    centerComponent={{ text: 'USTAWIENIA', style: { flex:1, color: '#fff', fontSize: 0.02 * height, alignItems: 'center',
+                            justifyContent: 'center',}}}
+                    containerStyle={{
+                    backgroundColor: '#bef7e8',
+                        height: 0.06 * height
+                }}
+                />
+                </ImageBackground>
+            </View>
+        );
+    }
+}
+
+export const PricesStack = createStackNavigator({Prices: PricesScreen, },
+    {defaultNavigationOptions: {
+            title: '',
+            headerRight: <Text style = {styles.header}>{newDate}</Text>,
+            headerRightContainerStyle: {
+                marginRight: 0.05 * width,
+                color: 'white',
+                fontSize: 0.02 * height
+            },
+            headerStyle: {
+                backgroundColor: '#55858A',
+                height: 0.07 * height,
+                width: width,
+                marginLeft: 0,
+                position: 'absolute',
+                zIndex: 1,
+                elevation: 50
+            },
+            headerTintColor: '#FFFFFF',
+            headerTitleStyle: {
+                textAlign:"center",
+                flex:1
+            },}})
+
+export const SettingsStack = createStackNavigator({Settings: SettingsScreen, },
+    {defaultNavigationOptions: {
+            title: '',
+            headerRight: <Text style = {styles.header}>{newDate}</Text>,
+            headerRightContainerStyle: {
+                marginRight: 0.05 * width,
+                color: 'white',
+                fontSize: 0.02 * height
+            },
+            headerStyle: {
+                backgroundColor: '#55858A',
+                height: 0.07 * height,
+                width: width,
+                marginLeft: 0,
+                position: 'absolute',
+                zIndex: 1,
+                elevation: 50
+            },
+            headerTintColor: '#FFFFFF',
+            headerTitleStyle: {
+                textAlign:"center",
+                flex:1
+            },}})
+
+export const StatsStack = createStackNavigator({Stats: StatsScreen, },
+    {defaultNavigationOptions: {
+            title: '',
+            headerRight: <Text style = {styles.header}>{newDate}</Text>,
+            headerRightContainerStyle: {
+                marginRight: 0.05 * width,
+                color: 'white',
+                fontSize: 0.02 * height
+            },
+            headerStyle: {
+                backgroundColor: '#55858A',
+                height: 0.07 * height,
+                width: width,
+                marginLeft: 0,
+                position: 'absolute',
+                zIndex: 1,
+                elevation: 50
+            },
+            headerTintColor: '#FFFFFF',
+            headerTitleStyle: {
+                textAlign:"center",
+                flex:1
+            },}})
+
+export const ProfileStack = createStackNavigator({Profile: ProfileScreen, },
+    {defaultNavigationOptions: {
+            title: '',
+            headerRight: <Text style = {styles.header}>{newDate}</Text>,
+            headerRightContainerStyle: {
+                marginRight: 0.05 * width,
+                color: 'white',
+                fontSize: 0.02 * height
+            },
+            headerStyle: {
+                backgroundColor: '#55858A',
+                height: 0.07 * height,
+                width: width,
+                marginLeft: 0,
+                position: 'absolute',
+                zIndex: 1,
+                elevation: 50
+            },
+            headerTintColor: '#FFFFFF',
+            headerTitleStyle: {
+                textAlign:"center",
+                flex:1
+            },}})
+
+export const TransactionStack = createStackNavigator({Profile: TransactionScreen, },
+    {defaultNavigationOptions: {
+            title: '',
+            headerRight: <Text style = {styles.header}>{newDate}</Text>,
+            headerRightContainerStyle: {
+                marginRight: 0.05 * width,
+                color: 'white',
+                fontSize: 0.02 * height
+            },
+            headerStyle: {
+                backgroundColor: '#55858A',
+                height: 0.07 * height,
+                width: width,
+                marginLeft: 0,
+                position: 'absolute',
+                zIndex: 1,
+                elevation: 50
+            },
+            headerTintColor: '#FFFFFF',
+            headerTitleStyle: {
+                textAlign:"center",
+                flex:1
+            },}})
+
+const sidebarNavigator = createSidebarNavigator(
+    {
+        Profile: ProfileStack,
+        Transaction: TransactionStack,
+        Stats: StatsStack,
+        Prices: PricesStack,
+        Settings: SettingsStack
+    },
+    {
+        initialRouteName: 'Profile',
+    },
+);
+
+export default createAppContainer(sidebarNavigator);
---
+++ PanBajgielAssistant/src/tabs/SidebarTabs.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { View, TouchableOpacity, Image, StyleSheet, Dimensions } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+var {width, height} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+    tab: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 0.14 * width,
+        height: 0.12 * height,
+        backgroundColor: 'transparent',
+        overflow: 'hidden',
+    },
+    tabContainerColor: {
+        height: height,
+        width: 0.14 * width,
+        backgroundColor: '#55858A',
+        position: 'absolute',
+        zIndex: 2,
+        elevation: 50
+    },
+    tabContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 0.6 * height,
+        width: 0.14 * width,
+        marginTop: 0.2 * height,
+        marginBottom: 0.2 * height,
+        position: 'absolute',
+        zIndex: 3,
+        elevation: 50
+    },
+    oneTab: {
+        width: 0.14 * width,
+        height: 0.12 * height,
+    },
+    stretch: {
+        height: 0.09 * height,
+        width: 0.09 * height,
+        marginTop: 0.05 * height,
+        marginLeft: 0.1 * width,
+        position: 'absolute'
+    },
+    abs: {
+        position: 'absolute',
+        zIndex: 5
+    }
+});
+
+const SidebarTabs = ({ navigation, descriptors }) => {
+    const { routes, index } = navigation.state;
+
+    return (
+        <View style = {styles.tabContainerColor}>
+            <View style = {styles.abs}>
+                <Image style={styles.stretch} source = {require("../../assets/nazwa.png")} />
+            </View>
+        <View style={styles.tabContainer}>
+            {routes.map((route, tabIndex) => {
+                const { routeName, params } = route;
+                let icon = 'cogs';
+                const color = tabIndex === index ? 'white' : '#bdb9b9';
+                if (routeName === 'Profile'){icon =  'user'}
+                else if (routeName === 'Transaction'){icon = 'money'}
+                else if (routeName === 'Stats'){icon = 'line-chart'}
+                else if (routeName === 'Prices'){icon = 'food-croissant'}
+                else {icon = 'cogs'}
+                return (
+                    <View>
+                    <View style = {styles.tab}>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate(routeName)}
+                        key={route.routeName}>
+                        <FontAwesome name={icon} size={0.05 *height} color={color} style={{ marginRight: 10 }} />
+                    </TouchableOpacity>
+                    </View>
+                    </View>
+                );
+            })}
+        </View>
+    </View>
+    );
+};
+
+export default SidebarTabs;
