81cea4a7bfc8317603d92e468b28b6a8d29dd5c3
Ania Wójcik <34057261+wojcikania12@users.noreply.github.com>
2020-01-11 23:59:28 +0100

    Add files via upload

--- PanBajgielBackend/src/main/java/com/Controllers/ProductController.java
+++ PanBajgielBackend/src/main/java/com/Controllers/ProductController.java
@@ -1,15 +1,15 @@
-package com.controllers;
+package com.Controllers;
 
+import com.models.Details;
 import com.models.Product;
 import java.util.List;
+
+import com.models.Transaction;
 import com.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -24,13 +24,37 @@ public class ProductController {
             allProductsFromDataBase = productRepository.getAllProductsFromDataBase();
         }
         else{
-            allProductsFromDataBase = productRepository.getProduct(productId);
+            allProductsFromDataBase = productRepository.findProducts(productId);
         }
         if(allProductsFromDataBase == null){
             return new ResponseEntity(HttpStatus.NOT_FOUND);
 
         } else {
             return new ResponseEntity<List<Product>>(allProductsFromDataBase,HttpStatus.OK);
+        }
+    }
+
+    @RequestMapping(value = "/transaction", method = RequestMethod.POST)
+    public ResponseEntity<Object> addTransaction(@RequestBody Transaction transcaction){
+        boolean action = productRepository.addNewTransaction(transcaction);
+        if(action) {
+            return new ResponseEntity(HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "/transaction_details", method = RequestMethod.POST )
+    public ResponseEntity<Object> addTransactionDetails(@RequestBody List<Details> details){
+        Integer transactionId = productRepository.getLastTranscatonId();
+        for(var detail : details){
+            detail.setTransactionId(transactionId);
+        }
+        boolean action = productRepository.addNewTransactionDetails(details);
+        if(action) {
+            return new ResponseEntity(HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
     }
 }
