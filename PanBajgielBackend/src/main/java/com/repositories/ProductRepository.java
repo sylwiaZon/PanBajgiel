62af2d113d15a9097794b99c8d2aaf1ae84e8814
Ania Wójcik <34057261+wojcikania12@users.noreply.github.com>
2020-01-11 23:57:43 +0100

    Add files via upload

--- PanBajgielBackend/src/main/java/com/repositories/ProductRepository.java
+++ PanBajgielBackend/src/main/java/com/repositories/ProductRepository.java
@@ -1,11 +1,9 @@
 package com.repositories;
 
-import com.models.Product;
-import com.models.ProductRowMapper;
+import com.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,12 +26,40 @@ public class ProductRepository {
         return productsFromDataBase;
     }
 
-    public List<Product> getProduct(String productId){
+    public List<Product> findProducts(String productId){
         String sql = "Select * from product where id in ( "+ productId +")";
         List<Product> productsFromDataBase = new ArrayList<Product>(jdbcTemplate.query(sql, new ProductRowMapper()));
         if(productsFromDataBase.isEmpty()) {
             return null;
         }
         return productsFromDataBase;
+    }
+
+    public boolean addNewTransaction(Transaction transaction){
+        var sql = String.format("INSERT INTO transaction(user_login,shop_id,date) VALUES ('%s', %d,'%s')",transaction.getUserLogin(), transaction.getShopId(),transaction.getDate());
+        jdbcTemplate.execute(sql);
+        return true;
+    }
+
+    public boolean addNewTransactionDetails(List <Details> details){
+        var sql = "INSERT INTO details(transaction_id,product_id,amount) VALUES  ";
+        for (var detail: details){
+            var detailString = String.format(" (%d,%d,%d),",detail.getTransactionId(),detail.getProductId(),detail.getAmount());
+            sql += detailString;
+        }
+        sql = sql.substring(0,sql.length()-1);
+        jdbcTemplate.execute(sql);
+        return true;
+    }
+
+    public Integer getLastTranscatonId(){
+        var sql = "SELECT  * from transaction ORDER BY id DESC LIMIT 1";
+        List<Transaction> lastTransaction = jdbcTemplate.query(sql, new TransactionRowMapper());
+        if(lastTransaction.size() > 0) {
+            Integer lastTransactionId = lastTransaction.get(0).getId();
+            return lastTransactionId;
+        } else {
+            return null;
+        }
     }
 }
